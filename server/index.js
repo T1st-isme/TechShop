0840417256a29170eb9003e5bf3112cf0a5b3748
Hà Trung Triển <trientrung102@gmail.com>
2023-09-28 11:14:47 +0700

    change branch

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { PORT, mongoUrl } from "./config.js";
 import mongoose from "mongoose";
+import { Person } from "./models/peopleModel.js";
+import cors from "cors";
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+
+//CORS
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(res);
@@ -13,8 +21,30 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-app.post("/peoples", (req, res) => {
+app.post("/persons", async (req, res) => {
   try {
+    // Check if request body is valid
+    if (!req.body.name || !req.body.age || !req.body.email) {
+      return res.status(400).send({ message: "Invalid body" });
+    }
+    // Create new person
+    const newPerson = {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    };
+    const person = await Person.create(newPerson);
+    return res.status(201).send(person);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/persons", async (req, res) => {
+  try {
+    const persons = await Person.find({});
+    return res.status(200).send(persons);
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: error.message });
