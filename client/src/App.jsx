17413695156c17b7c74d9f2d5e190bcdaf1129da
Hà Trung Triển <trientrung102@gmail.com>
2023-10-15 09:40:20 +0000

    add login/sigup layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/masterLayout.jsx";
 import Page1 from "./pages/Page1.jsx";
 import Page2 from "./pages/Page2.jsx";
+import Register from "./pages/Auth/Register/Register";
+import Login from "./pages/Auth/Login/Login";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Routes>
             <Route path="/Products" element={<Page1 />} />
             <Route path="/Pricing" element={<Page2 />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Login" element={<Login />} />
           </Routes>
         </div>
       </Layout>
--- client/src/components/Layout/masterLayout.jsx
+++ client/src/components/Layout/masterLayout.jsx
@@ -1,11 +1,9 @@
-import NavBar from "../NavBar/NavBar.jsx";
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
 
 const masterLayout = (props) => {
   return (
     <div>
-      <NavBar />
       <Header />
       <main>{props.children}</main>
       <Footer />
