027dc2048e9cadbbf6972567f1117587b1a2f441
Restyled.io <commits@restyled.io>
2023-12-26 10:38:56 +0000

    Restyled by clang-format

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-import express from "express";
-import { PORT, mongoUrl } from "./config.js";
-import mongoose from "mongoose";
-import { Person } from "./models/peopleModel.js";
 import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+import {mongoUrl, PORT} from "./config.js";
+import {Person} from "./models/peopleModel.js";
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(express.json());
 
-//CORS
+// CORS
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -17,27 +18,25 @@ app.get("/", (req, res) => {
   return res.status(234).send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
 
 app.post("/persons", async (req, res) => {
   try {
     // Check if request body is valid
     if (!req.body.name || !req.body.age || !req.body.email) {
-      return res.status(400).send({ message: "Invalid body" });
+      return res.status(400).send({message : "Invalid body"});
     }
     // Create new person
     const newPerson = {
-      name: req.body.name,
-      age: req.body.age,
-      email: req.body.email,
+      name : req.body.name,
+      age : req.body.age,
+      email : req.body.email,
     };
     const person = await Person.create(newPerson);
     return res.status(201).send(person);
   } catch (error) {
     console.log(error);
-    return res.status(500).send({ message: error.message });
+    return res.status(500).send({message : error.message});
   }
 });
 
@@ -47,15 +46,10 @@ app.get("/persons", async (req, res) => {
     return res.status(200).send(persons);
   } catch (error) {
     console.log(error);
-    return res.status(500).send({ message: error.message });
+    return res.status(500).send({message : error.message});
   }
 });
 
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Failed to connect to MongoDB", err);
-  });
+mongoose.connect(mongoUrl)
+    .then(() => { console.log("Connected to MongoDB"); })
+    .catch((err) => { console.log("Failed to connect to MongoDB", err); });
--- server/models/peopleModel.js
+++ server/models/peopleModel.js
@@ -1,23 +1,21 @@
 import mongoose from "mongoose";
 
-const personSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    age: {
-      type: Number,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
+const personSchema = mongoose.Schema({
+  name : {
+    type : String,
+    required : true,
   },
-  {
-    timestamps: true,
-  }
-);
+  age : {
+    type : Number,
+    required : true,
+  },
+  email : {
+    type : String,
+    required : true,
+  },
+},
+                                     {
+                                       timestamps : true,
+                                     });
 
 export const Person = mongoose.model("person", personSchema);
