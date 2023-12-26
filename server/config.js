55ef2c5921410da7ff691f6284bfbc3a0442ec72
Hà Trung Triển <triensteam102@gmail.com>
2023-12-26 17:38:25 +0700

    Revert "style: format code with Prettier and StandardJS"

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
-export const PORT = 4000
-export const mongoUrl = 'mongodb+srv://admin:admin@test.bvwirwc.mongodb.net'
+export const PORT = 4000;
+export const mongoUrl = "mongodb+srv://admin:admin@test.bvwirwc.mongodb.net";
